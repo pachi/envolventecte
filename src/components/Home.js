4610a3d53ad8f62d6989045df5baaf6b8cd1aa0c
Rafael Villar Burke <pachi@rvburke.com>
2016-12-08 13:00:32 +0100

    Mueve climas y otras constantes a Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,40 @@ import { Grid, Navbar, Jumbotron, Button } from 'react-bootstrap';
 
 import 'soljs';
 
+const ZONESLIST = ['A1c', 'A2c', 'A3c', 'A4c',
+                   'Alfa1c', 'Alfa2c', 'Alfa3c', 'Alfa4c',
+                   'B1c', 'B2c', 'B3c', 'B4c', 'C1c', 'C2c', 'C3c', 'C4c',
+                   'D1c', 'D2c', 'D3c', 'E1c',
+                   'A3', 'A4', 'B3', 'B4', 'C1', 'C2', 'C3', 'C4',
+                   'D1', 'D2', 'D3', 'E1'];
+
+// Orientaciones
+const ORIENTACIONES = [
+  // Area, slope, azimuth, name
+  { beta: 0, gamma: 0, name: 'Horiz.' },
+  { beta: 90, gamma: -135, name: 'NE' },
+  { beta: 90, gamma: -90, name: 'E' },
+  { beta: 90, gamma: -45, name: 'SE' },
+  { beta: 90, gamma: 0, name: 'S' },
+  { beta: 90, gamma: 45, name: 'SW' },
+  { beta: 90, gamma: 90, name: 'W' },
+  { beta: 90, gamma: 135, name: 'NW' },
+  { beta: 90, gamma: 180, name: 'N' }
+];
+
+const MESES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const components = [
+  {
+    active: true,
+    carrier: 'ELECTRICIDAD',
+    ctype: 'CONSUMO',
+    originoruse: 'EPB',
+    values: [9.67, 7.74, 4.84, 4.35, 2.42, 2.90, 3.87, 3.39, 2.42, 3.87, 5.80, 7.74],
+    comment: 'Linea 1 de ejemplo3PVBdC.csv'
+  }
+];
+
 const Home = inject("appstate")(
   observer(class Home extends Component {
     render() {
--- src/stores/AppState.js
+++ src/stores/AppState.js
@@ -1,12 +1,5 @@
 import { extendObservable, action } from 'mobx';
 
-const zoneslist = ['A1c', 'A2c', 'A3c', 'A4c',
-                   'Alfa1c', 'Alfa2c', 'Alfa3c', 'Alfa4c',
-                   'B1c', 'B2c', 'B3c', 'B4c', 'C1c', 'C2c', 'C3c', 'C4c',
-                   'D1c', 'D2c', 'D3c', 'E1c',
-                   'A3', 'A4', 'B3', 'B4', 'C1', 'C2', 'C3', 'C4',
-                   'D1', 'D2', 'D3', 'E1'];
-
 export default class AppState {
   constructor() {
     extendObservable(
