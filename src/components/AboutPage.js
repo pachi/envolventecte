47f79f9e8629297db346ad4b115153ce6be757f9
Rafael Villar Burke <pachi@ietcc.csic.es>
2017-03-07 17:43:36 +0100

    AboutPage: Introducir gráficas

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -22,16 +22,18 @@ SOFTWARE.
 */
 
 import React, { Component } from 'react';
-import { Button, Grid, Alert } from 'react-bootstrap';
+import { Alert, Button, Col, Grid, Row, Image } from 'react-bootstrap';
 
 import NavBar from './Nav';
+import esquema5R1C from './5R1C.svg';
+import orientaciones from './orientaciones.svg';
 
 export default class AboutPage extends Component {
   render() {
     return (
-      <div>
+      <Grid>
         <NavBar route={ this.props.route } />
-        <Grid>
+        <Row>
           <h1>Radiación solar en superficies orientadas e indicadores de envolvente térmica</h1>
           <p className="lead">Cálculos de radiación solar para la aplicación del CTE DB-HE</p>
           <Alert bsStyle="warning"><b>NOTA:</b> Esta aplicación y la información contenida en ella no tiene valor reglamentario.</Alert>
@@ -47,6 +49,13 @@ export default class AboutPage extends Component {
               Descargar climas de referencia de codigotecnico.org
             </Button>
           </p>
+        </Row>
+        <Row>
+          <Col md={8} mdOffset={2}>
+            <Image responsive alt="Roseta de orientaciones de superficies" src={ orientaciones } />
+          </Col>
+        </Row>
+        <Row>
           <h3>Indicadores de la envolvente térmica</h3>
           <p>Los indicadores de calidad en la envolvente térmica se basan en los descritos en la UNE EN ISO 13790:2008 (e ISO/FDIS 52016‐1).</p>
 
@@ -54,16 +63,19 @@ export default class AboutPage extends Component {
           <p>El indicador de <b>ganancias solares (q<sub>sol;jul</sub>)</b> se basa en el flujo de calor por ganancias solares (&Phi;<sub>sol;k</sub>, apartado 11.3.2, ec. 43 de la UNE EN ISO 13790:2008 y apartado 6.5.13.2, ec. 69 de la ISO/FDIS 52016-1), despreciando la reirradiación al cielo, y repercutido por la superficie útil considerada.</p>
 
           <p><small>En este último indicador no se han considerado de forma separada las componentes difusa y directa al tener en cuenta el efecto de las obstrucciones solares, siguiendo el criterio de la UNE EN ISO 13790:2008 y no la formulación de la ISO/FDIS 52016-1 (ver ec. 69, apartado 6.5.13.2).</small></p>
-        </Grid>
-        <Grid>
+          <Col md={6} mdOffset={3}>
+            <Image responsive alt="Esquema 5R1C EN 13790" src={ esquema5R1C } />
+          </Col>
+        </Row>
+        <Row>
           <h3>Equipo de desarrollo:</h3>
           <ul>
             <li>Rafael Villar Burke, <i>pachi@ietcc.csic.es</i></li>
             <li>Daniel Jiménez González, <i>danielj@ietcc.csic.es</i></li>
             <li>Marta Sorribes Gil, <i>msorribes@ietcc.csic.es</i></li>
           </ul>
-        </Grid>
-      </div>
+        </Row>
+      </Grid>
     );
   }
 };
