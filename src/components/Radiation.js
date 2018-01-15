8846f5d66e57bebb03621d991903c409b60cb8e8
Rafael Villar Burke <pachi@ietcc.csic.es>
2018-01-15 16:26:05 +0100

    Radiation: tabla de factores de reducción por sombras móviles

--- src/components/Radiation.js
+++ src/components/Radiation.js
@@ -84,6 +84,39 @@ const RadiationTable = ({ data }) =>
   </table>;
 
 
+const ShadingFactorsTable = ({ data }) =>
+  <table id="shadingfactorstable"
+    className="table table-striped table-bordered table-condensed">
+    <thead>
+      <tr style={{ borderBottom: '3px solid darkgray' }}>
+        <th className="col-md-1">Superficie</th>
+        <th className="col-md-1">f<sub>sh;with</sub></th>
+        <th>ENE</th><th>FEB</th><th>MAR</th><th>ABR</th>
+        <th>MAY</th><th>JUN</th><th>JUL</th><th>AGO</th>
+        <th>SET</th><th>OCT</th><th>NOV</th><th>DIC</th>
+      </tr>
+    </thead>
+    { data.map((d, idx) => {
+      return (
+        <tbody style={{ textAlign: 'right' }} key={'table' + idx}>
+          <tr key={'f_shwith200_' + d.surfname}>
+            <td rowSpan="3"><b>{d.surfname}</b></td>
+            <td>f<sub>sh;with,I>200</sub></td>
+            {d.f_shwith200.map((v, i) => <td key={'fshwith200_' + i}>{v.toFixed(2)}</td>)}
+          </tr>
+          <tr key={'f_shwith300_' + d.surfname} style={{ fontWeight: 'bold' }}>
+            <td>f<sub>sh;with,I>300</sub></td>
+            {d.f_shwith300.map((v, i) => <td key={'f_shwith300_' + i}>{v.toFixed(2)}</td>)}
+          </tr>
+          <tr key={'f_shwith500_' + d.surfname} style={{ borderBottom: '3px solid darkgray' }}>
+            <td>f<sub>sh;with,I>500</sub></td>
+            {d.f_shwith500.map((v, i) => <td key={'f_shwith500_' + i}>{v.toFixed(2)}</td>)}
+          </tr>
+        </tbody>);
+    })
+    }
+  </table>;
+
 const Radiation = inject("radstate")(observer(
   ({ radstate, route }) => {
     const { climatedata } = radstate;
@@ -106,11 +139,25 @@ const Radiation = inject("radstate")(observer(
                 horizontal y superficies verticales con la orientación indicada.</p>
               </Row>
             </Tab>
-            <Tab eventKey={3} title="Factores de reducción por sombreamiento (f_sh;with)">
+            <Tab eventKey={3} title="Factores de reducción por sombras móviles (f_sh;with)">
               <Row>
                 <h2>Factores mensuales de reducción para sombreamientos solares móviles</h2>
-                {/* <RadiationTable data={climatedata} /> */}
-                <p>La tabla anterior recoge la fracción del tiempo (mensual) que el dispositivo de sombra móvil está conectado.</p>
+                <ShadingFactorsTable data={climatedata} />
+                <p>La tabla anterior recoge la fracción del tiempo (mensual) que el dispositivo
+                de sombra móvil está conectado.</p>
+                <p>Estos valores pueden resultar útiles para obtener los valores del factor solar del hueco
+                teniendo en cuenta los dispositivos móviles, pudiendo promediar los valores mensuales al
+                aplicarlos a un periodo de tiempo superior (p.e. verano e invierno).</p>
+                <p>Se puede considerar que el dispositivo está conectado cuando la radiación (total) incidente
+                supera el valor indicado (<i>I > 200 W/m<sup>2</sup></i>, <i>I > 300 W/m<sup>2</sup></i>, <i>I > 500 W/m<sup>2</sup></i>)
+                y desconectado cuando se encuentra por debajo de ese valor.</p>
+                <p>Usos indicados:
+                  <ul>
+                  <li><i>I > 300 W/m<sup>2</sup></i>: dispositivos de sombra con accionamiento y control manual;</li>
+                  <li><i>I > 200 W/m<sup>2</sup></i>: dispositivos de sombra con control y accionamiento automatizado;</li>
+                  <li><i>I > 500 W/m<sup>2</sup></i>: dispositivos de sombra en modo de calefacción (evita cargas extremas).</li>
+                  </ul>
+                </p>
               </Row>
             </Tab>
           </Tabs>
