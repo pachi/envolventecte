2af1863c4e1c7ca3a48c54ce23e690d43b90f4c9
Rafael Villar Burke <pachi@ietcc.csic.es>
2020-07-22 19:39:38 +0200

    Updates

---
+++ precache-manifest.c4b15b5494e021e57efd98c2f5ee212a.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ef1dc54f8eeb6996dfd44e48656ac2a",
+    "url": "/envolventecte/index.html"
+  },
+  {
+    "revision": "d931c4785449e9181b89",
+    "url": "/envolventecte/static/css/2.541b0151.chunk.css"
+  },
+  {
+    "revision": "338a9df804933b738e1b",
+    "url": "/envolventecte/static/css/main.87ed6282.chunk.css"
+  },
+  {
+    "revision": "d931c4785449e9181b89",
+    "url": "/envolventecte/static/js/2.c18a3af9.chunk.js"
+  },
+  {
+    "revision": "bf2ebf32675cc9ea6b583af48978e9c0",
+    "url": "/envolventecte/static/js/2.c18a3af9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "338a9df804933b738e1b",
+    "url": "/envolventecte/static/js/main.32922a1c.chunk.js"
+  },
+  {
+    "revision": "e2c6407a43413afec331",
+    "url": "/envolventecte/static/js/runtime-main.54f747ff.js"
+  },
+  {
+    "revision": "5774dbba5adbee596edee2d0de9039e5",
+    "url": "/envolventecte/static/media/5R1C.5774dbba.svg"
+  },
+  {
+    "revision": "a4df5d9eab1d7dc3d6489425c6517ab2",
+    "url": "/envolventecte/static/media/FotoDani.a4df5d9e.jpg"
+  },
+  {
+    "revision": "9e5efdfa6e91ad9dfd6febbe9271d23b",
+    "url": "/envolventecte/static/media/FotoMarta.9e5efdfa.jpg"
+  },
+  {
+    "revision": "ea8ceed08e22aadea014796320a76de7",
+    "url": "/envolventecte/static/media/baseline-add-24px.ea8ceed0.svg"
+  },
+  {
+    "revision": "97ba759738cd11226a632ea3f51689ca",
+    "url": "/envolventecte/static/media/baseline-archive-24px.97ba7597.svg"
+  },
+  {
+    "revision": "6d8b9d96bc84c41f533eeb567abeb8ff",
+    "url": "/envolventecte/static/media/baseline-remove-24px.6d8b9d96.svg"
+  },
+  {
+    "revision": "7929a5bebf8f8d7d2933a38a7eb2aa46",
+    "url": "/envolventecte/static/media/logo.7929a5be.svg"
+  },
+  {
+    "revision": "be993caae0710cba0baf8c15a81dc0f2",
+    "url": "/envolventecte/static/media/orientaciones.be993caa.svg"
+  },
+  {
+    "revision": "9a40aa396be3f68fb855d2f563cb0603",
+    "url": "/envolventecte/static/media/outline-add_comment-24px.9a40aa39.svg"
+  },
+  {
+    "revision": "570f16232afb6e71393bdff51d55c524",
+    "url": "/envolventecte/static/media/outline-file_copy-24px.570f1623.svg"
+  },
+  {
+    "revision": "8a95ad69593d1380c4db13c5768522e7",
+    "url": "/envolventecte/static/media/outline-info-24px.8a95ad69.svg"
+  },
+  {
+    "revision": "e6acfc69d88b054557de6de28526b7a6",
+    "url": "/envolventecte/static/media/outline-live_help-24px.e6acfc69.svg"
+  }
+]);
