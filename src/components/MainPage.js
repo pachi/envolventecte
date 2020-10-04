fe3b189277227f2c63cd510e61f7054343b70a61
Rafael Villar Burke <pachi@ietcc.csic.es>
2020-10-04 20:47:56 +0200

    Main: convierte a componente funcional

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -30,65 +30,62 @@ import NavBar from "./Nav";
 
 import imglogo from "./img/logo.svg";
 
-const HelpPage = props => (
-      <Container fluid>
-        <NavBar route={props.route} />
-        <Row>
-          <Col md={3}>
-            <Image src={imglogo} width="90%" />
-          </Col>
-          <Col md={9}>
-            <h1>Envolvente CTE</h1>
-            <h3>
-              Parámetros de eficiencia energética de la envolvente térmica
-            </h3>
-            <p>
-              Esta aplicación facilita el cálculo de indicadores de calidad y
-              parámetros descriptivos de la envolvente térmica de los edificios
-              para su evaluación energética y para la aplicación del CTE DB-HE
-              (2019).
-            </p>
-            <p>
-              Puede comenzar introduciendo la{" "}
-              <b>descripción de la envolvente térmica y sus elementos</b> en el
-              apartado <Link to="/building">Edificio</Link> y seleccionando la{" "}
-              <b>zona climática</b> en el desplegable del menú superior.
-            </p>
-            <p>
-              El apartado <Link to="/building">Edificio</Link> muestra en un
-              panel el valor actualizado y el desglose del cálculo de la{" "}
-              <b>transmitancia térmica global</b> (<b>K</b>) y del{" "}
-              <b>parámetro de control solar</b> (
-              <b>
-                q<sub>sol;jul</sub>
-              </b>
-              ) de la envolvente definida.
-            </p>
-            <p>
-              Como ayuda para completar la descripción de la envolvente térmica
-              puede consultar:
-            </p>
-            <ul>
-              <li>
-                el apartado <Link to="/climate">Clima</Link>, para aquellos
-                parámetros relacionados con el clima exterior del edificio.
-              </li>
-              <li>
-                el apartado <Link to="/elements">Elementos</Link>, para
-                parámetros de elementos de la envolvente térmica que se pueden
-                obtener a partir de otros parámetros básicos.
-              </li>
-            </ul>
-          </Col>
-        </Row>
-        <Row>
-          <Alert variant="warning">
-            <b>NOTA:</b> Esta aplicación y la información contenida en ella no
-            tiene valor reglamentario.
-          </Alert>
-        </Row>
-        <Footer />
-      </Container>
-    );
+const HelpPage = (props) => (
+  <Container fluid>
+    <NavBar route={props.route} />
+    <Row>
+      <Col md={3}>
+        <Image src={imglogo} width="90%" />
+      </Col>
+      <Col md={9}>
+        <h1>Envolvente CTE</h1>
+        <h3>Parámetros de eficiencia energética de la envolvente térmica</h3>
+        <p>
+          Esta aplicación facilita el cálculo de indicadores de calidad y
+          parámetros descriptivos de la envolvente térmica de los edificios para
+          su evaluación energética y para la aplicación del CTE DB-HE (2019).
+        </p>
+        <p>
+          Puede comenzar introduciendo la{" "}
+          <b>descripción de la envolvente térmica y sus elementos</b> en el
+          apartado <Link to="/building">Edificio</Link> y seleccionando la{" "}
+          <b>zona climática</b> en el desplegable del menú superior.
+        </p>
+        <p>
+          El apartado <Link to="/building">Edificio</Link> muestra en un panel
+          el valor actualizado y el desglose del cálculo de la{" "}
+          <b>transmitancia térmica global</b> (<b>K</b>) y del{" "}
+          <b>parámetro de control solar</b> (
+          <b>
+            q<sub>sol;jul</sub>
+          </b>
+          ) de la envolvente definida.
+        </p>
+        <p>
+          Como ayuda para completar la descripción de la envolvente térmica
+          puede consultar:
+        </p>
+        <ul>
+          <li>
+            el apartado <Link to="/climate">Clima</Link>, para aquellos
+            parámetros relacionados con el clima exterior del edificio.
+          </li>
+          <li>
+            el apartado <Link to="/elements">Elementos</Link>, para parámetros
+            de elementos de la envolvente térmica que se pueden obtener a partir
+            de otros parámetros básicos.
+          </li>
+        </ul>
+      </Col>
+    </Row>
+    <Row>
+      <Alert variant="warning">
+        <b>NOTA:</b> Esta aplicación y la información contenida en ella no tiene
+        valor reglamentario.
+      </Alert>
+    </Row>
+    <Footer />
+  </Container>
+);
 
 export default HelpPage;
