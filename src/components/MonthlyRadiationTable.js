4070901a7656b377edc6ccdf1594dc686d52d9cc
Rafael Villar Burke <pachi@ietcc.csic.es>
2020-10-06 11:39:03 +0200

    MonthlyRadiationTable: convertir a componente funcional

--- src/components/MonthlyRadiationTable.js
+++ src/components/MonthlyRadiationTable.js
@@ -21,112 +21,107 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Col, Form, Row } from "react-bootstrap";
 
 import { OrientaIcon } from "./IconsOrientaciones";
 
-// Tabla de irradiación mensual acumulada para distintas superficies orientadas e inclinadas
-export default class MonthlyRadiationTable extends Component {
-  constructor(...args) {
-    super(...args);
-    this.MESES = "ENE,FEB,MAR,ABR,MAY,JUN,JUL,AGO,SET,OCT,NOV,DIC".split(",");
-    this.state = { showDetail: false };
-  }
+const MESES = "ENE,FEB,MAR,ABR,MAY,JUN,JUL,AGO,SET,OCT,NOV,DIC".split(",");
 
-  render() {
-    const { data } = this.props;
-    const showDetail = this.state.showDetail;
-    return (
-      <Col>
-        <Row>
-          <Col>
-            <h4>Irradiación solar (acumulada) mensual (kWh/m²/mes)</h4>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Form inline>
-              <Form.Check
-                checked={showDetail}
-                onChange={e => this.setState({ showDetail: !showDetail })}
-                label="¿Mostrar detalle de irradiación directa y difusa?"
-              />
-            </Form>
-          </Col>
-        </Row>
-        <Row style={{ marginTop: "2em" }}>
-          <Col>
-            <table
-              id="radiationtable"
-              className="table table-striped table-bordered table-condensed"
-            >
-              <thead>
-                <tr style={{ borderBottom: "3px solid darkgray" }}>
-                  <th className="col-md-1">Superficie</th>
-                  <th className="col-md-1">Irradiación</th>
-                  {this.MESES.map(m => (
-                    <th key={m}>{m}</th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody>
-                {data.map((d, idx) => {
-                  return (
-                    <React.Fragment key={idx}>
-                      <tr
-                        key={"tot_" + d.surfname}
-                        style={
-                          showDetail
-                            ? {
-                                fontWeight: "bold",
-                                borderTop: "3px solid darkgray"
-                              }
-                            : null
-                        }
-                      >
-                        <td rowSpan={showDetail ? "3" : null}>
-                          <b className="pull-left">{d.surfname}</b>
-                          <OrientaIcon dir={d.surfname} />
-                        </td>
-                        <td>Dir. + Dif.</td>
-                        {d.tot.map((v, i) => (
-                          <td key={"tot_" + i}>{v.toFixed(2)}</td>
+// Tabla de irradiación mensual acumulada para distintas superficies orientadas e inclinadas
+const MonthlyRadiationTable = ({ data }) => {
+  const [showDetail, setShowDetail] = useState(false);
+  return (
+    <Col>
+      <Row>
+        <Col>
+          <h4>Irradiación solar (acumulada) mensual (kWh/m²/mes)</h4>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Form inline>
+            <Form.Check
+              checked={showDetail}
+              onChange={(e) => setShowDetail(!showDetail)}
+              label="¿Mostrar detalle de irradiación directa y difusa?"
+            />
+          </Form>
+        </Col>
+      </Row>
+      <Row style={{ marginTop: "2em" }}>
+        <Col>
+          <table
+            id="radiationtable"
+            className="table table-striped table-bordered table-condensed"
+          >
+            <thead>
+              <tr style={{ borderBottom: "3px solid darkgray" }}>
+                <th className="col-md-1">Superficie</th>
+                <th className="col-md-1">Irradiación</th>
+                {MESES.map((m) => (
+                  <th key={m}>{m}</th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((d, idx) => {
+                return (
+                  <React.Fragment key={idx}>
+                    <tr
+                      key={"tot_" + d.surfname}
+                      style={
+                        showDetail
+                          ? {
+                              fontWeight: "bold",
+                              borderTop: "3px solid darkgray",
+                            }
+                          : null
+                      }
+                    >
+                      <td rowSpan={showDetail ? "3" : null}>
+                        <b className="pull-left">{d.surfname}</b>
+                        <OrientaIcon dir={d.surfname} />
+                      </td>
+                      <td>Dir. + Dif.</td>
+                      {d.tot.map((v, i) => (
+                        <td key={"tot_" + i}>{v.toFixed(2)}</td>
+                      ))}
+                    </tr>
+                    {showDetail ? (
+                      <tr key={"dir_" + d.surfname}>
+                        <td>Dir.</td>
+                        {d.dir.map((v, i) => (
+                          <td key={"dir_" + i}>{v.toFixed(2)}</td>
                         ))}
                       </tr>
-                      {showDetail ? (
-                        <tr key={"dir_" + d.surfname}>
-                          <td>Dir.</td>
-                          {d.dir.map((v, i) => (
-                            <td key={"dir_" + i}>{v.toFixed(2)}</td>
-                          ))}
-                        </tr>
-                      ) : null}
-                      {showDetail ? (
-                        <tr key={"dif_" + d.surfname}>
-                          <td>Dif.</td>
-                          {d.dif.map((v, i) => (
-                            <td key={"dif_" + i}>{v.toFixed(2)}</td>
-                          ))}
-                        </tr>
-                      ) : null}
-                    </React.Fragment>
-                  );
-                })}
-              </tbody>
-            </table>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <p className="text-info">
-              La tabla anterior recoge la radiación mensual acumulada para una
-              superficie horizontal y superficies verticales con la orientación
-              indicada.
-            </p>
-          </Col>
-        </Row>
-      </Col>
-    );
-  }
-}
+                    ) : null}
+                    {showDetail ? (
+                      <tr key={"dif_" + d.surfname}>
+                        <td>Dif.</td>
+                        {d.dif.map((v, i) => (
+                          <td key={"dif_" + i}>{v.toFixed(2)}</td>
+                        ))}
+                      </tr>
+                    ) : null}
+                  </React.Fragment>
+                );
+              })}
+            </tbody>
+          </table>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <p className="text-info">
+            La tabla anterior recoge la radiación mensual acumulada para una
+            superficie horizontal y superficies verticales con la orientación
+            indicada.
+          </p>
+        </Col>
+      </Row>
+    </Col>
+  );
+};
+
+export default MonthlyRadiationTable;
