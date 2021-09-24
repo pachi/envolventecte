bf3ee62f588e4ed908a6a91a110c52c505571ad4
Rafael Villar Burke <pachi@ietcc.csic.es>
2021-09-24 20:40:35 +0200

    Elimina opaco adyacente si las condiciones de contorno no son INTERIOR

--- src/components/building/OpacosTable.js
+++ src/components/building/OpacosTable.js
@@ -242,7 +242,10 @@ const OpacosTable = ({ selected, setSelected }) => {
           blurToSave: true,
           // Corrige el valor del espacio adyacente de "" a null
           afterSaveCell: (oldValue, newValue, row, column) => {
-            if (column.dataField === "nextto" && newValue === "") {
+            if (
+              (column.dataField === "nextto" && newValue === "") ||
+              row.bounds !== "INTERIOR"
+            ) {
               row.nextto = null;
             } else if (column.dataField === "A" && newValue !== "") {
               // Convierte a número campos numéricos
