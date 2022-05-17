1c4631702f8466452c55141d24f161b42ff9eafa
Rafael Villar Burke <pachi@ietcc.csic.es>
2022-05-17 17:51:16 +0200

    Corregir notas de tabla de espacios

--- src/components/building/SpacesTable.js
+++ src/components/building/SpacesTable.js
@@ -284,7 +284,7 @@ const SpacesTable = ({ selected, setSelected }) => {
       headerAlign: "center",
       headerFormatter: () => (
         <>
-          A<br />
+          A<sub>use;zt</sub><br />
           <span style={{ fontWeight: "normal" }}>
             <i>
               [m<sup>2</sup>]
