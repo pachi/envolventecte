cac31f6ffa77ea31e8c9142b12df790f54d265ab
Rafael Villar Burke <pachi@ietcc.csic.es>
2022-05-23 10:54:17 +0200

    Revert "Prueba actualización de react y react-scripts"
    This reverts commit 3d0937c952f2b11134f07d2c2c45e4b77be67368.

--- src/main.js
+++ src/main.js
@@ -3,9 +3,6 @@ import ReactDOM from "react-dom";
 import { App } from "./components/App";
 import "./index.css";
 
-// Investigar https://rustwasm.github.io/wasm-bindgen/examples/hello-world.html#indexjs
-// https://rustwasm.github.io/wasm-bindgen/examples/without-a-bundler.html
-// Ver --target web de wasm-bindgen
 import { set_panic_hook, set_log_hook } from "wasm-envolventecte";
 
 set_panic_hook();
