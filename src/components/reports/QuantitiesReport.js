065003bd2a16f5b48c6ab8523c7ab97c3003bafe
Rafael Villar Burke <pachi@ietcc.csic.es>
2022-10-28 14:59:14 +0200

    Mejora informe de mediciones

--- src/components/reports/QuantitiesReport.js
+++ src/components/reports/QuantitiesReport.js
@@ -22,29 +22,47 @@ SOFTWARE.
 */
 
 import React, { useContext, useRef } from "react";
-import { Col, Row, Button } from "react-bootstrap";
+import { Table, Col, Row, Button } from "react-bootstrap";
 import { observer } from "mobx-react";
 
 import { APP_VERSION } from "../../version.js";
 
 import AppState from "../../stores/AppState";
+import { round_or_dash } from "../../utils";
+import {
+  BOUNDARY_TYPES_MAP,
+  BOUNDARY_TYPES,
+  ORIENTATION_TYPES,
+} from "../../stores/types";
 
 import printer from "../img/print-solid.svg";
-import SpacesDetail from "./SpacesDetail.js";
 
 const QuantitiesReport = () => {
   const spaces_detail = useRef(null);
   const opaques_detail = useRef(null);
   const windows_detail = useRef(null);
-  const shades_detail = useRef(null);
-
-  const appstate = useContext(AppState);
-  const { area_ref, vol_env_net, vol_env_gross, compacity } =
-    appstate.energy_indicators;
-
-  const { name } = appstate.meta;
 
   const date = new Date(Date.now());
+
+  const appstate = useContext(AppState);
+
+  const { name } = appstate.meta;
+  const { area_ref, vol_env_net, vol_env_gross, compacity, props } =
+    appstate.energy_indicators;
+
+  // TODO: spaces
+
+  let wallData = computeRows(
+    appstate.cons.wallcons,
+    props.walls,
+    (e) => e.area_net * e.multiplier
+  );
+  let windowData = computeRows(
+    appstate.cons.wincons,
+    props.windows,
+    (e) => e.area * e.multiplier
+  );
+  let shadesData = computeShadesRows(props.shades);
 
   return (
     <>
@@ -156,21 +174,56 @@ const QuantitiesReport = () => {
             <li onClick={() => windows_detail.current.scrollIntoView()}>
               Huecos
             </li>
-            <li onClick={() => shades_detail.current.scrollIntoView()}>
-              Elementos de sombra
-            </li>
           </ol>
         </Col>
       </Row>
 
       <hr ref={spaces_detail} />
-      <SpacesDetail />
+      <Row className="mb-4 print-section">
+        <Col>
+          <h3 className="mb-4">Espacios</h3>
+          <p>
+            Espacios pertenecientes o no a la envolvente térmica (superficies
+            interiores).
+          </p>
+        </Col>
+      </Row>
+
       <hr ref={opaques_detail} />
-      {/* <UDetail /> */}
+      <Row className="mb-4 print-section">
+        <Col>
+          <h3 className="mb-4">Opacos</h3>
+          <p>
+            Elementos opacos pertenecientes o no a la envolvente térmica
+            (superficies netas).
+          </p>
+        </Col>
+      </Row>
+      <Row className="print-section">
+        <Col>
+          <h4>Muros, cubiertas, suelos y particiones interiores</h4>
+          <ByConceptTable data={wallData} />
+        </Col>
+      </Row>
+      <Row className="print-section">
+        <Col>
+          <h4>Elementos de sombra</h4>
+          <ShadesByConceptTable data={shadesData} />
+        </Col>
+      </Row>
+
       <hr ref={windows_detail} />
-      {/* <QSolJulDetail /> */}
-      <hr ref={shades_detail} />
-      {/* <N50Detail /> */}
+      <Row className="mb-4 print-section">
+        <Col>
+          <h3 className="mb-4">Huecos</h3>
+          <p>Huecos pertenecientes o no a la envolvente térmica.</p>
+        </Col>
+      </Row>
+      <Row className="print-section">
+        <Col>
+          <ByConceptTable data={windowData} />
+        </Col>
+      </Row>
       <div id="print-head">
         <h5>
           EnvolventeCTE ({APP_VERSION}) - {date.toLocaleString("es-ES")}
@@ -182,3 +235,198 @@ const QuantitiesReport = () => {
 };
 
 export default observer(QuantitiesReport);
+
+// Tabla de desglose por conceptos
+const ByConceptTable = ({ data }) => {
+  const total = data.reduce((acc, e) => (acc += e.area), 0);
+  return (
+    <Table
+      striped
+      bordered
+      hover
+      size="sm"
+      className="small"
+      style={{ margin: "0 auto", marginBottom: "2em", width: "70%" }}
+    >
+      <thead style={{ background: "lightGray" }}>
+        <tr>
+          <th>
+            Solución constructiva
+            <br />
+          </th>
+          <th>Condición de contorno</th>
+          <th>Orientación</th>
+          <th className="text-center" colSpan={2} style={{ width: "20%" }}>
+            A<br />
+            [m²]
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row, idx) => (
+          <React.Fragment key={`${idx}-${row.name}`}>
+            <tr style={{ height: "1em" }}></tr>
+            <tr style={{ borderTop: "2 solid black" }}>
+              <td>
+                <i>
+                  <b>{row.name}</b>
+                </i>
+              </td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td className="text-center">
+                <b>{round_or_dash(row.area)}</b>
+              </td>
+            </tr>
+            {row.children.map((row2, idx2) => (
+              <React.Fragment key={`${idx}-${idx2}`}>
+                <tr>
+                  <td></td>
+                  <td>
+                    <i>{BOUNDARY_TYPES_MAP[row2.bounds]}</i>
+                  </td>
+                  <td></td>
+                  <td className="text-center">
+                    <u>
+                      <i>{round_or_dash(row2.area)}</i>
+                    </u>
+                  </td>
+                  <td></td>
+                </tr>
+                {row2.children.map((row3, idx3) => (
+                  <tr key={`${idx}-${idx2}-${idx3}`}>
+                    <td></td>
+                    <td></td>
+                    <td>{row3.orientation}</td>
+                    <td className="text-center">{round_or_dash(row3.area)}</td>
+                    <td></td>
+                  </tr>
+                ))}
+              </React.Fragment>
+            ))}
+          </React.Fragment>
+        ))}
+        <tr style={{ height: "1em" }}></tr>
+        <tr style={{ background: "lightGray" }}>
+          <td>
+            <b>TOTAL</b>
+          </td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td className="text-center">
+            <b>{round_or_dash(total)}</b>
+          </td>
+        </tr>
+      </tbody>
+    </Table>
+  );
+};
+
+// Tabla de desglose por conceptos
+const ShadesByConceptTable = ({ data }) => {
+  const total = data.reduce((acc, e) => (acc += e.area), 0);
+  return (
+    <Table
+      striped
+      bordered
+      hover
+      size="sm"
+      className="small"
+      style={{ margin: "0 auto", marginBottom: "2em", width: "70%" }}
+    >
+      <thead style={{ background: "lightGray" }}>
+        <tr>
+          <th>Condición de contorno</th>
+          <th>Orientación</th>
+          <th className="text-center" style={{ width: "20%" }}>
+            A<br />
+            [m²]
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <b>Elementos de sombra</b>
+          </td>
+          <td></td>
+          <td className="text-center">
+            <b>{round_or_dash(total)}</b>
+          </td>
+        </tr>
+        {data.map((row, idx) => (
+          <tr key={idx}>
+            <td></td>
+            <td>{row.orientation}</td>
+            <td className="text-center">{round_or_dash(row.area)}</td>
+          </tr>
+        ))}
+        <tr style={{ height: "1em" }}></tr>
+        <tr style={{ background: "lightGray" }}>
+          <td>
+            <b>TOTAL</b>
+          </td>
+          <td></td>
+          <td className="text-center">
+            <b>{round_or_dash(total)}</b>
+          </td>
+        </tr>
+      </tbody>
+    </Table>
+  );
+};
+
+function computeRows(cons, props, adder = () => 1) {
+  let data = [];
+  for (const wc of cons) {
+    const id = wc.id;
+    const name = wc.name;
+    const consTotal = { name, area: 0, children: [] };
+    for (const bounds of BOUNDARY_TYPES) {
+      const boundTotal = { bounds, area: 0, children: [] };
+      for (const orientation of ORIENTATION_TYPES) {
+        const orientTotal = { orientation, area: 0 };
+        const selected = Object.values(props).filter(
+          (w) =>
+            w.orientation === orientation && w.bounds === bounds && w.cons == id
+        );
+        for (const elem of selected) {
+          const area = adder(elem);
+          consTotal.area += area;
+          boundTotal.area += area;
+          orientTotal.area += area;
+        }
+        if (orientTotal.area > 0.01) {
+          boundTotal.children.push(orientTotal);
+        }
+      }
+      if (boundTotal.area > 0.01) {
+        consTotal.children.push(boundTotal);
+      }
+    }
+    if (consTotal.area > 0.01) {
+      data.push(consTotal);
+    }
+  }
+  return data;
+}
+
+function computeShadesRows(props) {
+  let data = [];
+  for (const orientation of ORIENTATION_TYPES) {
+    const orientTotal = { orientation, area: 0 };
+    const selected = Object.values(props).filter(
+      (w) => w.orientation === orientation
+    );
+    for (const elem of selected) {
+      const area = elem.area;
+      orientTotal.area += area;
+    }
+    if (orientTotal.area > 0.01) {
+      data.push(orientTotal);
+    }
+  }
+  return data;
+}
