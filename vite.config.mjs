220d428ff961e71990936664b0f93657b2b9bf57
Rafael Villar Burke <pachi@ietcc.csic.es>
2026-04-26 12:51:09 +0200

    Corrige base según deploy o start

--- vite.config.mjs
+++ vite.config.mjs
@@ -6,7 +6,11 @@ import wasm from "vite-plugin-wasm";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  // el base path del repositorio para que funcione en github pages como si estuviera en la raíz
+  // del dominio debe ser '/envovlventecte/', pero para el desarrollo local debe ser '/'.
+  // Establecemos la ruta para producción en el predeploy de package.json
+  // Ver https://vite.dev/guide/static-deploy
+  base: process.env.VITE_BASE || '/',
   resolve: {
     // which allows you to import from folders under the /src folder
     // import Button from '~/components/Button';
@@ -16,7 +20,14 @@ export default defineConfig({
   },
   server: {
     // open browser on start
-    open: true,
+    open: true
+    // proxy: {
+    //   '/envolventecte': {
+    //     target: 'http://localhost:5173/',
+    //     // changeOrigin: true,
+    //     // secure: false,
+    //   },
+    // }
   },
   build: {
     outDir: 'build',
